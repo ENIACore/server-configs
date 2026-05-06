4c555e91a3b0edb2a7a19dde23a4b569d046014d
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2026-05-05 22:04:00 -0700

    [MINOR][PYTHON] Flesh out setup.py with directory creation and template-file copying

--- lib/setup.py
+++ lib/setup.py
@@ -61,3 +61,66 @@ def create_directories() -> None:
         MC_CONFIG_PATH,
         SERVER_CONFIG_PATH,
         QBIT_CONFIG_PATH,
+    ]
+
+    print_step("Creating configuration directories")
+    for directory in directories:
+        ensure_dir(str(directory))
+    print_group_end()
+
+
+def copy_template_files():
+    print_step("Copying template files to /etc/<service>/<file>.template...")
+
+    keys_path = SERVER_CLONE_PATH / "keys"
+    templates = [
+        (SERVER_CONFIG_PATH, "mlm.toml.template"),
+        (QBIT_CONFIG_PATH, "wg0.conf.template"),
+    ]
+
+    print_group_start("Copying template files")
+    for template in templates:
+        src = keys_path / template[1]
+        dest = Path(template[0]) / template[1]
+        copy_path(src, dest)
+        print_group_step(f"Copied template file {template[1]} to {dest}")
+    print_group_end(
+        f"{RED} Make sure to fill in values and remove .template tag for all template files{RESET}"
+    )
+
+
+def create_config():
+    print_step("Getting values for server configuration")
+    prompt_and_save(
+        "ROOT_DOMAIN", "Enter the root domain for the server including TLD"
+    )
+    root_domain = get_config_value("ROOT_DOMAIN")
+
+    print_info(f"Setting wildcard domain (*.{root_domain})")
+    set_config_value("WILDCARD_DOMAIN", f"*.{root_domain}")
+
+    print_info(f"Setting jellyfin subdomain (jelly.{root_domain})")
+    set_config_value("JELLY_SUBDOMAIN", f"jelly.{root_domain}")
+
+    print_info(f"Setting qbittorrent subdomain (qbit.{root_domain})")
+    set_config_value("QBIT_SUBDOMAIN", f"qbit.{root_domain}")
+
+    print_info(f"Setting vaultwarden subdomain (vault.{root_domain})")
+    set_config_value("VAULT_SUBDOMAIN", f"vault.{root_domain}")
+
+    print_info(f"Setting nextcloud subdomain (nextcloud.{root_domain})")
+    set_config_value("NEXTCLOUD_SUBDOMAIN", f"nextcloud.{root_domain}")
+
+    print_info(f"Setting server user ({SERVER_USER})")
+    set_config_value("USER", f"{SERVER_USER}")
+
+    print_info(f"Setting essential services path (/mnt/essential)")
+    print_info(
+        f"Essential services are the most important services used (vaultwarden)"
+    )
+    set_config_value("ESSENTIAL_SERVICES_PATH", "/mnt/essential")
+
+    print_info(f"Setting essential core path (/mnt/core)")
+    print_info(
+        f"Core services are services used daily (minecraft, nextcloud)"
+    )
