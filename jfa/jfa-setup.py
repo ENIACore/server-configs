2157acdab3ef5ca7697c6cde21802352430b34bd
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2026-09-14 20:36:00 -0700

    [MINOR][PYTHON] Flesh out jfa-setup.py with the config directory setup and start of the container run

--- jfa/jfa-setup.py
+++ jfa/jfa-setup.py
@@ -16,3 +16,21 @@ def main():
     media_path = require_config_value("MEDIA_SERVICES_PATH")
 
     require_dir(media_path, "Media services path")
+
+    jfa_config_dir = f"{media_path}/jfa/config"
+    jelly_config_dir = f"{media_path}/jelly/config"
+
+    print_info(f"Jellyfin config dir: {jelly_config_dir}")
+
+    print_step("Creating JFA directories...")
+    ensure_dir(jfa_config_dir)
+
+    ensure_network()
+
+    run_container(
+        name="jfa-go",
+        opts=[
+            "--network",
+            DOCKER_NETWORK_NAME,
+            "--volume",
+            f"{jfa_config_dir}:/data",
